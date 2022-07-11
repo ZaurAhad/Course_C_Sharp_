c62a3ccb92c5898ec8991b0c7461a879e9c53041
Zaringit <zarin-zarin@mail.ru>
2022-07-11 19:09:30 +0300

    Home Task 1_2

---
+++ 1_Lesson/Home/1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber == secondNumber) 
+{
+    Console.WriteLine("Числа равны");
+}
+else if (firstNumber > secondNumber) 
+{
+    Console.WriteLine("Первое число является максимальным");
+}
+else 
+{
+    Console.WriteLine("Второе число является максимальным");
+}
