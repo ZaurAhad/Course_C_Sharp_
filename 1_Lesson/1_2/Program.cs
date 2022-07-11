412d0cc354b043d4f49901a4921453c045e07191
Zaringit <zarin-zarin@mail.ru>
2022-07-11 14:50:44 +0300

    Task 1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b * b)
+Console.WriteLine("Да");
+else
+Console.WriteLine("Нет");
