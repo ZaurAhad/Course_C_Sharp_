5858a0d8a1030dccbdbe59325b3ba5d9fcd991e8
Zaringit <zarin-zarin@mail.ru>
2022-08-07 19:05:12 +0300

    Task 4_3

---
+++ 4_Lesson/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите прграмму, которая выводит массив из 8 элементов, запонленный нулями и единицами в случайном порядке
+
+void Random01Array(int num) 
+{
+    int[] array = new int[num];
+
+    for(int i = 0; i < num; i++) 
+    {
+        array[i] = new Random().Next(0, 2);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Random01Array(8);
