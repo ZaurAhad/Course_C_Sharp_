5ae073c6a22a66e636b4810f3f6296d55c42c1fe
Zaringit <zarin-zarin@mail.ru>
2022-08-07 18:33:06 +0300

    Task 4_1

---
+++ 4_Lesson/4_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, котора принимает на вход число и выдает количество цифр в числе
+
+void Length(int a) 
+{
+    string num = a.ToString();
+    Console.WriteLine(num.Length);
+}
+Length(223434);
