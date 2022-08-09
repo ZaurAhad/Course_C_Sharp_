8fd004a59cbf3cab767e578b85be998b3fff6ea0
Zaringit <zarin-zarin@mail.ru>
2022-08-09 09:25:11 +0300

    Home task 4_2

---
+++ 4_Lesson/Home/4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Sum(int a) 
+{
+    string num = a.ToString();
+    int result = 0;
+    int temp = 0;
+
+    for(int i = 0; i < num.Length; i++) 
+    {
+        temp = a % 10;
+        result += temp;
+        a = a / 10;       
+    }
+    return result;
+}
+
+
+Console.WriteLine(Sum(452));
+Console.WriteLine(Sum(82));
+Console.WriteLine(Sum(9012));
