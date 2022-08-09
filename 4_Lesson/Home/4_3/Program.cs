d9cc600bf06eeaa694783883ed5473dea21e56b3
Zaringit <zarin-zarin@mail.ru>
2022-08-09 09:40:56 +0300

    Home task 4_3

---
+++ 4_Lesson/Home/4_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void Arr()
+{
+    int[] array = new int[8];
+
+    for(int i = 0; i < array.Length; i++) 
+    {
+        array[i] = i + 1;
+        Console.Write($"{array[i]} ");
+    }
+    
+}
+    
+Arr();
