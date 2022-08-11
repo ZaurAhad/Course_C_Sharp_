65f951d071f2008e7a86342a47a1d64e879a75e6
Zaringit <zarin-zarin@mail.ru>
2022-08-11 18:59:30 +0300

    Task 5_3

---
+++ 5_Lesson/5_3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10, 99]
+
+int[] CreateArray()
+{
+    Console.WriteLine("Input number of elements");
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input element min");
+    int min = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input element max");
+    int max = int.Parse(Console.ReadLine());
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int Nums(int[] array) 
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+            count++;
+    }
+    return count;
+}
+ int[] arr_1 = CreateArray();
+ Print(arr_1);
+ Console.WriteLine(Nums(arr_1)); 
