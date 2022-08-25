1bc1bf635015c1a8a74d085f444add032f0bfd5f
Zaringit <zarin-zarin@mail.ru>
2022-08-25 21:17:20 +0300

    Task 6_1

---
+++ 6_Lesson/Home/6_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива
+// с помощью поэлементного копирования (использование дробных чисел)
+
+void Print(double[] arr) 
+{
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double[] MassNums(int size) 
+{
+    double[] arr = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Math.Round(new Random().NextDouble(), 2);
+    }
+    return arr;
+}
+
+double[] CopyMass(double[] arr) 
+{
+    double[] copyArr = new double[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copyArr[i] = arr[i];
+    }
+
+    return copyArr;
+}
+
+double[] array = MassNums(10);
+Print(array);
+double[] copyArray = CopyMass(array);
+Print(copyArray);
+
