8adbd7befaa66e3c0ac11f49fc91d5a7d1f99146
Zaringit <zarin-zarin@mail.ru>
2022-08-25 21:52:11 +0300

    Home task 6_2

---
+++ 6_Lesson/Home/6_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел больше 0 ввел пользователь.
+    
+    Console.WriteLine("Сколько чисел вы введете: ");
+    int size = int.Parse(Console.ReadLine());
+    int count = 0;
+    int i = 0;
+
+    while (i < size) 
+    {
+        
+        Console.WriteLine("Введите число: ");
+        int a = int.Parse(Console.ReadLine());
+        if (a > 0)
+            count++;
+            i++;
+    }
+
+    Console.WriteLine($"Вы ввели {count} чисел больше нуля");
