8ecc1838a8c1e93f2128cabe3b55f2d96a8dca26
Zaringit <zarin-zarin@mail.ru>
2022-08-28 16:13:13 +0300

    Home task 7_1

---
+++ 7_Lesson/Home/7_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m x n, заполненный случайными вещественными числами
+
+void Print(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ", 11);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MassNums(int row, int column)
+{
+    double[,] arr = new double[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = Math.Round(new Random().NextDouble() * new Random().Next(-10, 10), 1);
+        }
+    }
+    return arr;
+}
+
+double[,] arr_1 = MassNums(3, 4);
+Print(arr_1);
